24a498b2d4507f5c3a9840d344a3bc219a693a0f
Gabriel Gama <gabrielgamagga@gmail.com>
2023-10-25 17:17:29 -0400

    feat: adiciona projeto base e o caso de uso criar Candidato

---
+++ src/main/java/br/com/gabrielgama/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,42 @@
+package br.com.gabrielgama.gestao_vagas.modules.candidate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Entity(name ="candidate")
+public class CandidateEntity {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  
+  private String name;
+
+  @NotBlank
+  @Pattern(regexp = "\\S+", message="O campo [usernae] não deve conter espaço")
+  private String username;
+  
+  @Email(message = "O campo [email] deve conter um e-mail válido")
+  private String email;
+  
+  @Length(min=10, max=100)
+  private String password;
+  private String description;
+  private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
