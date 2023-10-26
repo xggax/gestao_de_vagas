d92ff2bcbf593bb733a506dea7e53a9b775f6bb4
Gabriel Gama <gabrielgamagga@gmail.com>
2023-10-25 20:29:47 -0400

    fix: readiciona JobEntity

---
+++ src/main/java/br/com/gabrielgama/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,34 @@
+package br.com.gabrielgama.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name = "job")
+public class JobEntity {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String description;
+  private String benefits;
+  private String level;
+
+  @ManyToOne()
+  @JoinColumn(name="company_id", insertable = false, updatable = false)
+  private CompanyEntity companyEntity;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
