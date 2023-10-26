eb3b47aa777bdcc7e8fdbc304fce18cf45f57e13
Gabriel Gama <gabrielgamagga@gmail.com>
2023-10-25 20:56:20 -0400

    feat: adiciona JobController-CreateJobUseCase-JobRepository

--- src/main/java/br/com/gabrielgama/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/gabrielgama/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,6 +29,9 @@ public class JobEntity {
   @ManyToOne()
   @JoinColumn(name="company_id", insertable = false, updatable = false)
   private CompanyEntity companyEntity;
+
+  @Column(name="company_id", nullable = false)
+  private UUID companyId;
 
   @CreationTimestamp
   private LocalDateTime createdAt;
